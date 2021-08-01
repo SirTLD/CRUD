2c4c395b00efebaaebe60cbfb848207779d02e99
SirTLD <ekanemita@gmail.com>
2021-08-01 18:16:41 -0500

    update to layout

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-const POST_DIVS = document.querySelector('.post');
+// const POST_DIVS = document.querySelector('.post');
 
-const URL_LINK =
-  'https://newsapi.org/v2/top-headlines?country=us&apiKey=30b0b06e6e744426829935a494ac6c1c';
+// const URL_LINK =
+//   'https://newsapi.org/v2/top-headlines?country=us&apiKey=30b0b06e6e744426829935a494ac6c1c';
 
-getNews(URL_LINK);
+// getNews(URL_LINK);
 
-async function getNews(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  //   showResults(data);
-  console.log(data);
-}
+// async function getNews(url) {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   //   showResults(data);
+//   console.log(data);
+// }
 
-console.log(getNews);
+// console.log(getNews);
