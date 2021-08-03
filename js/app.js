cb6d2521d79df9ccc1b8c80e1427bcfeb4c745c1
SirTLD <ekanemita@gmail.com>
2021-08-02 19:07:07 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-import productdb, { bulkcreate } from './module.js';
-
-let db = productdb('Productdb', {
-  products: `++id, name, seller, price`,
-});
-
 const userId = document.getElementById('user-id');
 const productName = document.getElementById('product-name');
 const seller = document.getElementById('seller');
@@ -13,12 +7,3 @@ const createBtn = document.getElementById('btn-create');
 const readBtn = document.getElementById('btn-read');
 const updateBtn = document.getElementById('btn-update');
 const deleteBtn = document.getElementById('btn-delete');
-
-createBtn.onclick = (event) => {
-  let flag = bulkcreate(db.products, {
-    name: productName.value,
-    seller: seller.value,
-    price: price.value,
-  });
-  console.log(flag);
-};
