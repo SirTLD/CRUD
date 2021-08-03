a80c024212f2086489fbb0c744367929e7805558
SirTLD <ekanemita@gmail.com>
2021-08-02 22:38:20 -0500

    update eto layout

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
-const userId = document.getElementById('user-id');
-const productName = document.getElementById('product-name');
-const seller = document.getElementById('seller');
-const price = document.getElementById('price');
+const createBtn = document.getElementById('add');
 
-const createBtn = document.getElementById('btn-create');
-const readBtn = document.getElementById('btn-read');
-const updateBtn = document.getElementById('btn-update');
-const deleteBtn = document.getElementById('btn-delete');
+createBtn.addEventListener('click', () => addEntry());
+
+let addEntry = (text = '') => {
+  const entry = document.createElement('div');
+  entry.classList.add('entry');
+  entry.innerHTML = `
+
+  <div class="card shadow-sm p-2">
+  <div class="card-body">
+      <div class="d-flex tools justify-content-end gap-2">
+          <button class="btn btn-outline-warning"><i class="far fa-edit btn-edit"></i></button>
+          <button class="btn btn-outline-warning"> <i class="far fa-trash-alt btn-delete"></i></button>
+      </div>
+      <h3 class="card-title">Card title</h3>
+
+      <div class="${text ? '' : 'hidden'} main">
+          <textarea class="${
+            text ? 'hidden' : ''
+          }"card-text main">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, assumenda. Nam
+              saepe tenetur cum aperiam corporis ducimus excepturi illo? Harum!</textarea>
+      </div>                        
+      <a href="#" class="btn btn-warning">Go somewhere</a>
+  </div>
+</div>
+
+
+
+  `;
+
+  let cardParent = document.getElementById('card-container-body');
+  cardParent.appendChild(entry);
+};
