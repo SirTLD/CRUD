89fb8a309b1477f125fd6a482e78d3c451e6d67d
SirTLD <ekanemita@gmail.com>
2021-08-02 23:44:39 -0500

    update to layout

--- js/app.js
+++ js/app.js
@@ -10,25 +10,41 @@ let addEntry = (text = '') => {
   <div class="card shadow-sm p-2">
   <div class="card-body">
       <div class="d-flex tools justify-content-end gap-2">
-          <button class="btn btn-outline-warning"><i class="far fa-edit btn-edit"></i></button>
-          <button class="btn btn-outline-warning"> <i class="far fa-trash-alt btn-delete"></i></button>
+          <button class="btn btn-warning"><i class="far fa-edit btn-edit"></i></button>
+          <button class="btn btn-warning"> <i class="far fa-trash-alt btn-delete"></i></button>
       </div>
-      <h3 class="card-title">Card title</h3>
-
+     
+      <input class='card-title'type="text" placeholder="Title">
       <div class="${text ? '' : 'hidden'} main">
-          <textarea class="${
-            text ? 'hidden' : ''
-          }"card-text main">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, assumenda. Nam
-              saepe tenetur cum aperiam corporis ducimus excepturi illo? Harum!</textarea>
+          <textarea class="${text ? 'hidden' : ''}"card-text main">
+
+         
+          
+          </textarea>
       </div>                        
       <a href="#" class="btn btn-warning">Go somewhere</a>
   </div>
 </div>
-
-
-
   `;
+
+  const deleteBtn = entry.querySelector('.btn-delete');
+  const editBtn = entry.querySelector('.btn-edit');
+  const main = entry.querySelector('.main');
+  const textArea = entry.querySelector('textarea');
+
+  deleteBtn.addEventListener('click', () => {
+    entry.remove();
+  });
 
   let cardParent = document.getElementById('card-container-body');
   cardParent.appendChild(entry);
 };
+
+function updateStorage() {
+  let notesText = document.querySelectorAll('textarea');
+  let notes = [];
+
+  notesText.forEach((note) => notes.push(note.value));
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+}
